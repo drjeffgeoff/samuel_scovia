d2481051b00181eb73b765e3ae1c37dfb2981f89
drjeffgeoff <jeff.geoff.phd@gmail.com>
2025-03-24 12:37:33 +0300

    Added for loop

---
+++ looping.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+// Looping constructs (for, while, do-while)
+
+// Loop 1 to 10
+
+int main()
+{
+    for (int i = 0; i <= 10; i++)
+    {
+        printf("%d\n", i);
+    }
+    return 0;
+}
