7a62634a1ee0380798e4efa0948670b1ca844ab6
drjeffgeoff <jeff.geoff.phd@gmail.com>
2025-03-24 12:12:26 +0300

    Committed the code for nested if

--- nestedif.c
+++ nestedif.c
@@ -1,1 +1,14 @@
 #include <stdio.h>
+
+int main()
+{
+    int x = 10, y = 5;
+    if (x > 5)
+    {
+        if (y > 4)
+        {
+            printf("Both condition are true \n");
+        }
+    }
+    return 0;
+}
