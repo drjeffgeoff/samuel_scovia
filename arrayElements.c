82445e3df801db43c4605b819a96e73fa73f1dc1
drjeffgeoff <jeff.geoff.phd@gmail.com>
2025-03-31 12:14:10 +0300

    Added

---
+++ arrayElements.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    int sarah[5] = {0, 1, 2, 3, 4};
+    // Accessing elements in the name sarah
+    printf("Third element: %d\n", sarah[2]);
+
+    return 0;
+}
---
+++ arrays.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    // Delcaration
+    int numbers[7];                         // Unintialised arrays
+    int primes[5] = {2, 3, 5, 7, 11, 13};   // Initialised arrays
+    int scores[5] = {10, 15};               // Partial initialization of arrays
+    int temps[] = {36.5, 37.0, 38.7, 39.8}; // size reffered arrays
+}
+
+// An array is a collection of similar data items
+// stored in contiguous memory locations.
+
+/*
+Arrays store multiple values of the same type.
+*/
